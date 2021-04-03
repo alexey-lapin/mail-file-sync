1c3ed7323bf7d23e18f32a91a4308f2dda1c3f0f
Alexey Lapin <alexey-lapin@protonmail.com>
2021-04-03 21:51:52 +0300

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         kotlin("jvm") version settings.extra["version.kt"] as String
         kotlin("kapt") version settings.extra["version.kt"] as String
         id("io.micronaut.library") version "1.2.0"
-        id("com.diffplug.spotless") version "5.11.0"
+        id("com.diffplug.spotless") version "5.11.1"
         id("com.github.ben-manes.versions") version "0.38.0"
         id("com.github.johnrengelman.shadow") version "6.1.0"
     }
